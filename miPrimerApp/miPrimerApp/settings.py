f6eed4fb8a89da33737c3b217c415de46baf089f
spenagos93 <santipenagos93@gmail.com>
2023-05-30 10:46:06 -0500

    se crean los migrates y un dato de ejemplo

--- miPrimerApp/miPrimerApp/settings.py
+++ miPrimerApp/miPrimerApp/settings.py
@@ -30,14 +30,24 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+)
+
+THIRD_APPS = (
+)
+
+LOCAL_APPS = (
+    'polls.apps.PollsConfig',
+)
+
+
+INSTALLED_APPS = THIRD_APPS + DJANGO_APPS  + LOCAL_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',

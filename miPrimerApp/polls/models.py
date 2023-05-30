935a11d609e592445f8cf9055f5c905737680d0f
spenagos93 <santipenagos93@gmail.com>
2023-05-30 11:10:56 -0500

    se modifica atributo pub_date del modelo choices

--- miPrimerApp/polls/models.py
+++ miPrimerApp/polls/models.py
@@ -1,10 +1,24 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
+#modelos o clases del ORM
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
+    pub_date = models.DateTimeField("date published" ,auto_now_add=True)
+    
+    #metodo str similar al toString de otros lenguaes de programación
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    
+    #metodo str similar al toString de otros lenguaes de programación
+    def __str__(self):
+        return self.choice_text

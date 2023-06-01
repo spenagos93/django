5250413a606e42dcc736702c7ceef85f90ec2221
spenagos93 <santipenagos93@gmail.com>
2023-06-01 13:20:53 -0500

    se crean formularios y generic views

--- miPrimerApp/polls/urls.py
+++ miPrimerApp/polls/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
 from . import views
 #rutas de la app polls
+
 app_name = "polls"
+
 urlpatterns = [
     #example: /polls
-    path("", views.index, name="index"),
+    path("", views.IndexView.as_view(), name="index"),
     #example: /polls/5/
-    path("<int:question_id>/details", views.details, name="details"),
+    path("<int:pk>/details", views.DetailsView.as_view(), name="details"),
     #example: /polls/5/results/
-    path("<int:question_id>/results", views.results, name="results"),
+    path("<int:pk>/results", views.ResultView.as_view(), name="results"),
     #example: /polls/5/votes/
     path("<int:question_id>/vote", views.vote, name="vote")
-
 ]
--- miPrimerApp/polls/views.py
+++ miPrimerApp/polls/views.py
@@ -1,18 +1,53 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from .models import Question, Choice
 # Create your views here.
+# estás visa esta hechas sin usar las generic views
+# def index(request):
+#     latest_question_list = Question.objects.all()
+#     return render(request, "polls/index.html", {"latest_question_list":latest_question_list})
 
-def index(request):
-    latest_question_list = Question.objects.all()
-    return render(request, "polls/index.html", {"latest_question_list":latest_question_list})
+# def details(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/details.html", {"question": question})
 
-def details(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/details.html", {"question": question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {
+#         "question": question
+#     })
 
-def results(request, question_id):
-    return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")
+#aca se usan las generic views
+
+class IndexView(generic.ListView):
+    context_object_name = "latest_question_list"
+    template_name = "polls/index.html"
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
+
+class DetailsView (generic.DetailView):
+    model = Question
+    template_name = "polls/details.html"
+
+class ResultView (generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
 
 def vote(request, question_id):
-    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, "polls/details.html", {
+            "question": question,
+            "error_message": "No elegiste una respuesta"
+        } )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
